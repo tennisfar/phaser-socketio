811d6835ce6ed2039fe2f38603a56ce0e8a2de78
Michael L Petersen <ekmlpe@danskespil.dk>
2019-02-08 12:11:40 +0100

    gold!

--- public/js/game.js
+++ public/js/game.js
@@ -24,6 +24,7 @@ var game = new Phaser.Game(config)
 function preload() {
   this.load.image('ship', 'assets/spaceShips_001.png')
   this.load.image('otherPlayer', 'assets/enemyBlack5.png')
+  this.load.image('star', 'assets/star_gold.png')
 }
 
 function create() {
@@ -60,6 +61,28 @@ function create() {
   })
 
   this.cursors = this.input.keyboard.createCursorKeys()
+
+  this.blueScoreText = this.add.text(16, 16, '', {
+    fontSize: '32px',
+    fill: '#0000FF'
+  })
+  this.redScoreText = this.add.text(584, 16, '', {
+    fontSize: '32px',
+    fill: '#FF0000'
+  })
+
+  this.socket.on('scoreUpdate', function (scores) {
+    self.blueScoreText.setText('Blue: ' + scores.blue)
+    self.redScoreText.setText('Red: ' + scores.red)
+  })
+
+  this.socket.on('starLocation', function (starLocation) {
+    if (self.star) self.star.destroy()
+    self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star')
+    self.physics.add.overlap(self.ship, self.star, function () {
+      this.socket.emit('starCollected')
+    }, null, self)
+  })
 }
 
 function update() {
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io').listen(server)
 const players = {}
+var star = {
+  x: Math.floor(Math.random() * 700) + 50,
+  y: Math.floor(Math.random() * 500) + 50
+}
+var scores = {
+  blue: 0,
+  red: 0
+}
 
 app.use(express.static(__dirname + '/public'))
 
@@ -25,6 +33,11 @@ io.on('connection', function (socket) {
   // send the players object to the new player
   socket.emit('currentPlayers', players)
 
+  // send the star object to the new player
+  socket.emit('starLocation', star)
+  // send the current scores
+  socket.emit('scoreUpdate', scores)
+
   // update all other players of the new player
   socket.broadcast.emit('newPlayer', players[socket.id])
 
@@ -40,12 +53,24 @@ io.on('connection', function (socket) {
 
   // when a player moves, update the player data
   socket.on('playerMovement', function (movementData) {
-    players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    players[socket.id].rotation = movementData.rotation;
+    players[socket.id].x = movementData.x
+    players[socket.id].y = movementData.y
+    players[socket.id].rotation = movementData.rotation
     // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
-  });
+    socket.broadcast.emit('playerMoved', players[socket.id])
+  })
+
+  socket.on('starCollected', function () {
+    if (players[socket.id].team === 'red') {
+      scores.red += 10
+    } else {
+      scores.blue += 10
+    }
+    star.x = Math.floor(Math.random() * 700) + 50
+    star.y = Math.floor(Math.random() * 500) + 50
+    io.emit('starLocation', star)
+    io.emit('scoreUpdate', scores)
+  })
 })
 
 server.listen(8081, function () {
