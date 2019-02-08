3f1b420100052525e88ee53f64ef3c8d6238b543
Michael L Petersen <ekmlpe@danskespil.dk>
2019-02-08 12:05:49 +0100

    i like to move it move it

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io').listen(server)
+const players = {}
 
 app.use(express.static(__dirname + '/public'))
 
@@ -11,8 +12,39 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   console.log('a user connected')
+
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  }
+
+  // send the players object to the new player
+  socket.emit('currentPlayers', players)
+
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id])
+
   socket.on('disconnect', function () {
     console.log('user disconnected')
+
+    // remove this player from our players object
+    delete players[socket.id]
+
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id)
+  })
+
+  // when a player moves, update the player data
+  socket.on('playerMovement', function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
   });
 })
 
